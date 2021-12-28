5023f9a6d48666ab863825744c9617bf6636484a
Sachin <sacharora99@gmail.com>
2021-12-28 14:15:09 +0530

    JSP Filters Demo Project

---
+++ src/main/java/CompFilter.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class CompFilter
+ */
+@WebFilter("/add")
+public class CompFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public CompFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		String name = req.getParameter("name");
+		PrintWriter out = response.getWriter();
+		if(name.length() >2)
+			chain.doFilter(request, response);
+		else
+			out.println("Name is short");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
